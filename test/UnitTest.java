f894f834f065db4ca7b318cfb2b5327207297a4d
aruquense <aruquense@gmail.com>
2017-11-15 15:03:14 +0100

    tests

--- test/UnitTest.java
+++ test/UnitTest.java
@@ -1,6 +1,8 @@
 import akka.actor.ActorSystem;
 import controllers.AsyncController;
 import controllers.CountController;
+import io.jsonwebtoken.lang.Assert;
+
 import org.junit.Test;
 import play.mvc.Result;
 import scala.concurrent.ExecutionContextExecutor;
@@ -9,6 +11,7 @@ import java.util.concurrent.CompletionStage;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
+import static org.junit.Assert.*;
 import static play.test.Helpers.contentAsString;
 
 /**
@@ -31,25 +34,43 @@ public class UnitTest {
         Result result = controller.count();
         assertThat(contentAsString(result)).isEqualTo("49");
     }
-
-    // Unit test a controller with async return
-    @Test
-    public void testAsync() {
-        final ActorSystem actorSystem = ActorSystem.create("test");
-        try {
-            final ExecutionContextExecutor ec = actorSystem.dispatcher();
-            final AsyncController controller = new AsyncController(actorSystem, ec);
-            final CompletionStage<Result> future = controller.message();
-
-            // Block until the result is completed
-            await().until(() -> {
-                assertThat(future.toCompletableFuture()).isCompletedWithValueMatching(result -> {
-                    return contentAsString(result).equals("Hi!");
-                });
-            });
-        } finally {
-            actorSystem.terminate();
-        }
+    
+    @Test 
+    public void Test(){
+    	int x = 5;
+    	int y = 5;
+    	assertEquals(x,y);
     }
+    
+    @Test 
+    public void Test3(){
+    	int x = 5;
+    	int y = 4;
+    	assertTrue(x>y) ;
+    }
+    
+    
+    
+    @Test
+    public void test2(){
+    	int a = 1 + 3;
+    	assertThat(a).isEqualTo(4);
+    }
+    @Test
+    public void test4(){
+    	String[] aass={"aa","ss"};
+    	String[] bbss={"aa","ss"};
+    	
+    	assertArrayEquals(aass,bbss);
+    }
+    
+    @Test
+    public void Test5()
+    {
+    	String str1 = "Niriqsha";
+    	String str2 = "Anurag";
+    	assertNotSame(str1,str2);
+    }
+    // Unit test a controller with async return
 
 }
